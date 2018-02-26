e9412fe101b4e6532d1cfb54e2dd06f4b2a920a6
muzeyc <237238520@qq.com>
2018-02-26 14:23:56 +0800

    1.08 eclipse one 测试版

--- MuzeyWeb/src/main/java/com/muzey/web/config/SpringMvcConfig.java
+++ MuzeyWeb/src/main/java/com/muzey/web/config/SpringMvcConfig.java
@@ -7,13 +7,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @EnableAspectJAutoProxy
 @ComponentScan("com.muzey.web.controller,com.muzey.web.base.proxy")
-public class SpringMvcConfig extends WebMvcConfigurerAdapter {
+public class SpringMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -30,7 +30,7 @@ public class SpringMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        
+
         //registry.addInterceptor(new ControllerProxy()).addPathPatterns("/*").addPathPatterns("/*/*");
     }
 }
