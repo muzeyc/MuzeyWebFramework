2780d23f6948e30d8947151f2f670100485bcedb
zhangmm1018 <31465846@qq.com>
2018-03-31 00:41:42 +0800

    zhangmm

--- MuzeyWeb/src/main/java/com/muzey/web/model/req/Sys008_PhotoInfoReqModel.java
+++ MuzeyWeb/src/main/java/com/muzey/web/model/req/Sys008_PhotoInfoReqModel.java
@@ -2,17 +2,26 @@ package com.muzey.web.model.req;
 
 public class Sys008_PhotoInfoReqModel extends RequestModelBase {
     private String name;
-
+    private int searchType;
     
     public String getName() {
     
         return name;
     }
-
     
     public void setName(String name) {
     
         this.name = name;
+    }
+    
+    public int getSearchType() {
+    
+        return searchType;
+    }
+    
+    public void setSearchType(int searchType) {
+    
+        this.searchType = searchType;
     }
     
 }
--- MuzeyWeb/src/main/webapp/Resource/Js/Page/Sys008_PhotoInfo.js
+++ MuzeyWeb/src/main/webapp/Resource/Js/Page/Sys008_PhotoInfo.js
@@ -2,17 +2,24 @@
     .controller('Sys008_PhotoInfo', function ($scope, netRequest) {
     	$scope.condition = {};
     	$scope.condition.searchType = 1;
-
 					    	
     	$scope.searchTypeSelect = function(type) {
-    		$scope.condition.searchType = type;
+    		if ($scope.condition.searchType != type) {
+        		$scope.condition.searchType = type;
+        		init();
+    		}
 		}
     	
+    	$scope.imageClick = function (photo){
+    		$scope.$broadcast("showPhotoEdit", photo);
+    	}
+    	
     	var init = function() {
-        	var req = {};
+        	var req = angular.copy($scope.condition);
         	req.offset = 0;
         	req.size = 50;
     		netRequest.post("/MuzeyWeb/Sys008_PhotoInfo", req, function (res) {
+    			$scope.typeList = res.typeList;
     			$scope.photoList = res.photoList;
             });
 		}
@@ -32,3 +39,67 @@
             }
         });
     }])
+    .directive('photoedit', function (netRequest, dialog, validate) {
+        return {
+            scope: {
+                afterCommit: "&"
+            },
+            controller: ['$scope', function ($scope) {
+            	$scope.typeList = [ {
+					subId : "0",
+					name : "本地图片"
+				}, {
+					subId : "1",
+					name : "网络图片"
+				} ];
+            	
+            	$scope.usetypeList = [ {
+					subId : "00",
+					name : "系统"
+				}, {
+					subId : 01,
+					name : "广告图片"
+				} , {
+					subId : 02,
+					name : "商品图片"
+				} , {
+					subId : 03,
+					name : "用户头像"
+				} , {
+					subId : 04,
+					name : "商户照片"
+				}];
+            	
+                $scope.cancel = function () {
+                    $scope.show = false;
+                }
+
+                $scope.commit = function () {
+                    if (!validate.doValidate("#validateEdit")) {
+                        return;
+                    }
+                    if ($scope.mode == "new") {
+                        if (!$scope.serialNoList || $scope.serialNoList.length <= 0) {
+                            dialog.showDialog("error", "请录入加工序列号！", {});
+                            return;
+                        }
+                    }
+
+                    var req = angular.copy($scope.photo);
+                    netRequest.post("/MuzeyWeb/Sys008_PhotoInfo/" + $scope.mode, req, function (res) {
+                        $scope.show = false;
+                        if ($scope.afterCommit) {
+                            $scope.afterCommit({ res: res });
+                        }
+                    });
+                }
+            }],
+            templateUrl: 'View/P000SysManage/Sys008_PhotoInfoEdit.html?v=' + Math.random(),
+            link: function ($scope, iElm, iAttrs, controller) {
+                $scope.$on("showPhotoEdit", function (event, photo) {
+                	$scope.show = true;
+                    $scope.photo = angular.copy(photo);
+                });
+            }
+        };
+    });
