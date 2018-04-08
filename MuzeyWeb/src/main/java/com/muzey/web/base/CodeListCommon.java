4cdd548a75a27923902d5d6e8018aff2f527ff17
zhangmm1018 <31465846@qq.com>
2018-04-08 22:42:09 +0800

    zhangmm

--- MuzeyWeb/src/main/java/com/muzey/web/base/CodeListCommon.java
+++ MuzeyWeb/src/main/java/com/muzey/web/base/CodeListCommon.java
@@ -1,7 +1,9 @@
 package com.muzey.web.base;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.base.DBHelper;
 import com.data.DataRow;
@@ -12,41 +14,75 @@ import com.muzey.web.model.CodeListModel;
 
 public class CodeListCommon extends MuzeyService {
 
-	private static DBHelper dbHelper = new DBHelper();
+    private static DBHelper dbHelper = new DBHelper();
 
-	/**
-	 * 根据父级查询所有子集数据
-	 * 
-	 * @param parentid
-	 * @return
-	 */
-	public static List<CodeListModel> GetCodeListChilden(String parentid) {
+    /**
+     * 根据父级查询所有子集数据
+     * 
+     * @param parentid
+     * @return
+     */
+    public static List<CodeListModel> GetCodeListChilden(String parentid) {
 
-		List<CodeListModel> codeListRes = null;
+        List<CodeListModel> codeListRes = new ArrayList<CodeListModel>();
 
-		StringBuffer sbSQL = new StringBuffer();
-		sbSQL.append(" SELECT name,codename ");
-		sbSQL.append(" FROM sys_codelist ");
-		sbSQL.append(" WHERE 1=1 ");
-		sbSQL.append(" AND parentid='" + parentid + "'");
-		DataTable codelistDatatable = dbHelper.sqlQuery(sbSQL.toString());
+        StringBuffer sbSQL = new StringBuffer();
+        sbSQL.append(" SELECT name,codename ");
+        sbSQL.append(" FROM sys_codelist ");
+        sbSQL.append(" WHERE 1=1 ");
+        sbSQL.append(" AND parentid='" + parentid + "'");
+        DataTable codelistDatatable = dbHelper.sqlQuery(sbSQL.toString());
 
-		if (CheckUtil.isNotNullOrEmpty(codelistDatatable)) {
+        if (CheckUtil.isNotNullOrEmpty(codelistDatatable)) {
 
-			codeListRes = new ArrayList<CodeListModel>();
+            for (int i = 0; i < codelistDatatable.getRowSize(); i++) {
 
-			for (int i = 0; i < codelistDatatable.getRowSize(); i++) {
+                CodeListModel codeList = new CodeListModel();
 
-				CodeListModel codeList = new CodeListModel();
+                DataRow dataRow = codelistDatatable.getRow(i);
 
-				DataRow dataRow = codelistDatatable.getRow(i);
+                codeList.setName(dataRow.getData("name"));
+                codeList.setCodename(dataRow.getData("codename"));
+                codeListRes.add(codeList);
+            }
+        }
 
-				codeList.setName(dataRow.getData("name"));
-				codeList.setCodename(dataRow.getData("codename"));
-				codeListRes.add(codeList);
-			}
-		}
+        return codeListRes;
+    }
 
-		return codeListRes;
-	}
+    /**
+     * 根据父级查询所有子集数据
+     * 
+     * @param parentid
+     * @return
+     */
+    public static Map<String, CodeListModel> GetCodeMap(String parentid) {
+
+        Map<String, CodeListModel> codeListRes = new HashMap<String, CodeListModel>();
+
+        StringBuffer sbSQL = new StringBuffer();
+        sbSQL.append(" SELECT name,codename ");
+        sbSQL.append(" FROM sys_codelist ");
+        sbSQL.append(" WHERE 1=1 ");
+        sbSQL.append(" AND parentid='" + parentid + "'");
+        DataTable dt = dbHelper.sqlQuery(sbSQL.toString());
+
+        if (CheckUtil.isNotNullOrEmpty(dt)) {
+
+            for (int i = 0; i < dt.getRowSize(); i++) {
+
+                DataRow dataRow = dt.getRow(i);
+                String codename = dataRow.getData("codename");
+                if (!codeListRes.containsKey(codename)) {
+
+                    CodeListModel model = new CodeListModel();
+                    model.setName(dataRow.getData("name"));
+                    model.setCodename(codename);
+                    codeListRes.put(codename, model);
+                }
+            }
+        }
+
+        return codeListRes;
+    }
 }
